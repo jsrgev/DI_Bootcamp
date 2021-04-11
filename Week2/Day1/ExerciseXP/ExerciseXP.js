4f9000a0de3e79a39c39370263d44e4966c4d4a7
Josh Regev <jsrgev@gmail.com>
2021-04-11 13:01:38 +0300

    exercise xp

---
+++ Week2/Day1/ExerciseXP/ExerciseXP.js
@@ -0,0 +1,13 @@
+5 + "34"	// 39	X	"534"
+5 - "4"		// 1	✓
+10 % 5	// 0	✓
+5 % 10	// 0	X	5
+"Java" + "Script"	// "JavaScript"	✓
+" " + " "	// "  "	✓
+" " + 0	// error	X	" 0"
+true + true // true			X	2
+true + false	// false	X	1
+false + true	// false	X	1
+false - true	// error	X	-1
+3 - 4	// -1	✓
+"Bob" - "bill"	// NaN	✓
