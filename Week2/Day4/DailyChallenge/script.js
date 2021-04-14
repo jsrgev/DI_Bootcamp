d3a75e77049bb19f0f135a6646971b74c5fb4ada
Josh Regev <jsrgev@gmail.com>
2021-04-15 00:26:19 +0300

    daily challenge

---
+++ Week2/Day4/DailyChallenge/script.js
@@ -0,0 +1,24 @@
+let sentence = prompt("Please enter several words.");
+let array = sentence.split(" ");
+
+let longest = array[0];
+
+for (i = 1; i<array.length; i++) {
+	if (array[i].length > longest) {
+		longest = array[i]
+	} else {
+		continue;
+	}
+}
+
+let stars = []
+
+for (word of array) {
+	let spaces = " ".repeat(longest.length-word.length);
+	let line = `* ${word}${spaces} *`
+	stars.push(line);
+}
+let bookend = "*".repeat(longest.length+4);
+stars.unshift(bookend);
+stars.push(bookend);
+console.log(stars.join("\n"));
