66df6bfdaa87b29ac2d7ce1112346e86e42d0573
Josh Regev <jsrgev@gmail.com>
2021-04-18 12:04:36 +0300

    rudimentary calculator functioning

--- Week2/Day5/Calculator/main.js
+++ Week2/Day5/Calculator/main.js
@@ -1,17 +1,25 @@
+let num1;
+let num2;
+let oper;
+let result;
+
 function number(num) {
-	let num1 = num;
-	console.log(num1);
-	return number;
+	if (num1 == undefined) {
+		num1 = num;
+	} else {
+		num2 = num;
+	}
+	console.log(`${num1} and ${num2}`);
 }
 
 
 function operator(operator) {
-	let oper = operator;
+	oper = operator;
 	console.log(oper);
-	return oper;
 }
-
+	
 
 function equal() {
-
+	result = eval(num1 + oper + num2);
+	console.log(result);
 }
