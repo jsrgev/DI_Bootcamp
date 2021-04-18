10518d5b83baf9af3bb1685288abfde84b23ae9c
Josh Regev <jsrgev@gmail.com>
2021-04-18 20:07:30 +0300

    calculator improvements

--- Week2/Day5/Calculator/main.js
+++ Week2/Day5/Calculator/main.js
@@ -3,24 +3,39 @@ let num2;
 let oper;
 let result;
 
+
 function number(num) {
-	if (num1 == undefined || oper == undefined) {
-		num1 = num;
+	if (num1 == undefined) {
+		if (oper != undefined) {
+			num1 = result;
+			num2 = num;
+			console.log(`${num1} and ${oper} and ${num2}`);
+		} else {
+			num1 = num;
+		}
+	} else if (oper == undefined) {
+		num1 = num1.toString().concat(num.toString());
 	} else {
-		num2 = num;
+		if (num2 == undefined) {
+			num2 = num;
+		} else {
+			num2 = num2.toString().concat(num.toString());
+		}
 	}
-	console.log(`${num1} and ${num2}`);
+	console.log(`${num1} and ${oper} and ${num2}`);
 }
 
 function operator(operator) {
-	oper = operator;
-	console.log(oper);
+	if (num1 != undefined || (num1 == undefined && result != undefined)) {
+		oper = operator;
+		console.log(oper);
+	}
 }
-
 
 function equal() {
 	result = eval(num1 + oper + num2);
-	num1 = result;
+	num1 = undefined;
+	num2 = undefined;
 	oper = undefined;
 	console.log(result);
 }
@@ -30,8 +45,14 @@ function sign() {
 }
 
 function percent() {
-	
+
 }
 
 
-
+function clearAll() {
+	num1 = undefined;
+	num2 = undefined;
+	oper = undefined;
+	result = undefined;
+	console.log(`${num1} and ${oper} and  ${num2}`);
+}
