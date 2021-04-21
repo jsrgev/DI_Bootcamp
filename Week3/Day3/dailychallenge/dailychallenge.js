81a6ed2dce96acd960f16dc072dc618c7974ffa0
Josh Regev <jsrgev@gmail.com>
2021-04-21 18:55:12 +0300

    daily challenge

---
+++ Week3/Day3/dailychallenge/dailychallenge.js
@@ -0,0 +1,19 @@
+// Instructions
+// Daily challenge : Show only the letters
+
+//     Create an input type text that takes/shows only letters.
+
+// Hint: use the keyup event listener and remove any character that is not a letter.
+// Hint : Check out keycodes in Javascript or Regular Expressions
+
+
+let input = document.querySelector("#input");
+input.addEventListener("keydown",lettersOnly);
+
+function lettersOnly(event) {
+	let key = event.keyCode;
+	if (key < 65 || key >90) {
+		event.preventDefault();
+	}
+}
+
