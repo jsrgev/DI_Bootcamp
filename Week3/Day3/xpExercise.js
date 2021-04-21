8fb4b07e644bcc0a9b52057ab0c9fa32e51a03d4
Josh Regev <jsrgev@gmail.com>
2021-04-21 15:23:49 +0300

    xp ex 2

--- Week3/Day3/xpExercise.js
+++ Week3/Day3/xpExercise.js
@@ -20,17 +20,52 @@ function myMove() {setInterval(function() {
 };
 
 
-// function myMove() {
-// id();
-// };
+// Exercise 2: Drag & Drop
+// Instructions
+
+//     Create a draggable square/box element in your HTML file.
+//     In your javascript file add the functionality which will allow you to drag the square/box and drop it into a larger box.
 
 
-// let id = setInterval(function() {
-// 	if (unit <= 350) {
-// 	box.style.left = unit + "px";
-// 	box.style.top = unit + "px";
-// 	unit++;
-// 	} else {
-// 		clearInterval(id);
-// 	}
-// }, 5)
+let drag = document.querySelector("#drag");
+
+let container = document.querySelector("#container");
+
+drag.addEventListener("dragstart", dragStart);
+
+function dragStart() {
+	console.log("a");
+	event.dataTransfer.setData("text",event.target.id);
+}
+
+
+container.addEventListener("dragover",allowDrop);
+container.addEventListener("drop",dragDrop);
+container.addEventListener("dragenter", dragEnter);
+container.addEventListener("dragleave", dragLeave);
+
+function allowDrop(event) {
+	event.preventDefault()
+}
+
+
+function dragDrop(event) {
+	event.preventDefault();
+	let dataDiv = event.dataTransfer.getData("text");
+	let box = document.getElementById(dataDiv);
+	event.target.appendChild(box);
+}
+
+function dragEnter(event){
+  event.target.style.backgroundColor = "aquamarine"
+}
+
+function dragLeave(event){
+  event.target.style.backgroundColor = "red"
+}
+
+
+
+
+
+
