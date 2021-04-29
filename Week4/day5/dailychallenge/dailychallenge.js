afe96fd7372b178e27e6e781344cc8408e8bbc2a
Josh Regev <jsrgev@gmail.com>
2021-04-29 14:09:36 +0300

    daily challenge

--- Week4/day5/dailychallenge/dailychallenge.js
+++ Week4/day5/dailychallenge/dailychallenge.js
@@ -16,11 +16,27 @@
 //     Do we need to consider whitespace?
 //     Trim whitespace prior to comparison.
 
-const anagramCheck = (string1,strin2) => {
-	let array1 = string1.replaceAll(" ","").split("");
-	let array2 = string2.replaceAll(" ","").split("");
-	for (letter of array1) {
-		let array3 = array2.filter(i => i=letter);
-		console.log(array3);
+const anagramCheck = (string1,string2) => {
+	let string1Tidy = string1.replaceAll(" ","").toLowerCase();
+	let string2Tidy = string2.replaceAll(" ","").toLowerCase();
+	let array1 = string1Tidy.split("").sort();
+	let array2 = string2Tidy.split("").sort();
+	if (array1.length !== array2.length) {
+		return false;
 	}
+	for (i = 0; i<array1.length; i++) {
+		if (array1[i] !== array2[i]) {
+			return false;
+		}
+	};
+	return true;
 }
+
+console.log(anagramCheck("The Morse Code","He re  come dots"));
+
+
+
+
+
+
+
