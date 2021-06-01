41a4c07c0f2fc61f7abbbd435b7e53973c56efd4
Josh Regev <jsrgev@gmail.com>
2021-06-01 21:06:09 +0300

    xp2

--- week8/day2/exercises/src/App.js
+++ week8/day2/exercises/src/App.js
@@ -1,35 +1,61 @@
 import React from 'react';
 
 
+class Child extends React.Component {
+  componentWillUnmount = () => {
+    alert("The header is about to be deleted");
+  }
+  render(){
+    return <h2>Hello world!</h2>;
+  }
+}
+
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      favoriteColor:'red'
+      favoriteColor:'red',
+      show: true
     }
   }
   changeColor = () => {
     this.setState({favoriteColor:'yellow'});
   }
+  deleteHeader = () => {
+    this.setState({show:false});
+  }
   componentDidMount = () => {
     setTimeout(this.changeColor, 5000);
   }
   clickHandler = () => {
-    this.setState({favoriteColor:'blue'})
+    // this.setState({favoriteColor:'blue'})
     this.setState({favoriteColor:'pink'})
   }
+  componentDidUpdate = () => {
+    let div = document.getElementById("message");
+    div.textContent = `My updated favorite color is ${this.state.favoriteColor}`
+  }
   shouldComponentUpdate = () => {
-    return false;
+    return true;
+    // return false;
+  }
+  getSnapshotBeforeUpdate = (prevProps, prevState) => {
+    let div = document.getElementById("snapshot");
+    div.textContent = `Before the update, the favorite was ${prevState.favoriteColor}.`
   }
   render(){
-    console.log()
-      return(
-        <>
+    let hello = this.state.show ? <Child /> : "";
+    return(
+      <>
+        {hello}
+        <button onClick={this.deleteHeader}>Delete header</button>
         <h1>My favorite color is {this.state.favoriteColor}.</h1>
-        <button onClick={this.clickHandler}>Change color</button>
-        </>
-      )
+        <button onClick={this.clickHandler}>Change color</button> 
+        <div id="snapshot"></div>
+        <div id="message"></div>
+      </>
+    )
   }
 }
 
