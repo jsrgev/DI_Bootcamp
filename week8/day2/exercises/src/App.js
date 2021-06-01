933421a86ae960b53e971131eaa0850c75850bf9
Josh Regev <jsrgev@gmail.com>
2021-06-01 22:48:45 +0300

    xp3

--- week8/day2/exercises/src/App.js
+++ week8/day2/exercises/src/App.js
@@ -16,9 +16,11 @@ class App extends React.Component {
     super();
     this.state = {
       favoriteColor:'red',
-      show: true
+      show: true,
+      isToggleOn: true
     }
-  }
+    this.shoot3 = this.shoot3.bind(this);
+    }
   changeColor = () => {
     this.setState({favoriteColor:'yellow'});
   }
@@ -28,10 +30,10 @@ class App extends React.Component {
   componentDidMount = () => {
     setTimeout(this.changeColor, 5000);
   }
-  clickHandler = () => {
-    // this.setState({favoriteColor:'blue'})
-    this.setState({favoriteColor:'pink'})
-  }
+  // clickHandler = () => {
+  //   // this.setState({favoriteColor:'blue'})
+  //   this.setState({favoriteColor:'pink'})
+  // }
   componentDidUpdate = () => {
     let div = document.getElementById("message");
     div.textContent = `My updated favorite color is ${this.state.favoriteColor}`
@@ -44,6 +46,39 @@ class App extends React.Component {
     let div = document.getElementById("snapshot");
     div.textContent = `Before the update, the favorite was ${prevState.favoriteColor}.`
   }
+  greatShot =() =>{
+    alert("Great shot!");
+  }
+  shoot = () => {
+    alert("Shoot Again!");
+
+  }
+  shoot2 = () => {
+    alert(this);
+
+  }
+  shoot3 = () => {
+    alert(this);
+
+  }
+  shoot4 = (string) => {
+    alert(string)
+  }
+  shoot5 = (string) => {
+    console.log("5");
+    alert(string)
+  }
+  clickHandler = () => {
+    alert("I was clicked")
+  }
+  handleKeyPress = (e) => {
+    e.key === "Enter" && alert(e.target.value);
+  }
+  toggleButton = () => {
+        this.state.isToggleOn ? this.setState({isToggleOn:false}) : this.setState({isToggleOn:true})
+        let toggler = document.getElementById("toggler");
+        toggler.textContent = this.state.isToggleOn;
+  }
   render(){
     let hello = this.state.show ? <Child /> : "";
     return(
@@ -54,6 +89,16 @@ class App extends React.Component {
         <button onClick={this.clickHandler}>Change color</button> 
         <div id="snapshot"></div>
         <div id="message"></div>
+        <button onClick={this.greatShot}>Great shot</button> 
+        <button onClick={this.shoot}>Shoot again</button> 
+        <button onClick={this.shoot2}>this</button> 
+        <button onClick={this.shoot3}>Keep Shooting!</button> <br />
+        <button onClick={() => {this.shoot4("Goal!")}}>Goal!</button> 
+        {/*<button onClick={() => {this.shoot5.bind(this,"Shooting!")}}>Shooting!</button> */}
+        <button onClick={(e) => this.shoot5("Shooting!", e)}>Shooting!</button>
+        <button onClick={this.clickHandler}>Click me!</button><br />
+        <input type="text" onKeyPress={this.handleKeyPress} /><br />
+        <button type="button" id="toggler" onClick={this.toggleButton}>true</button>
       </>
     )
   }
